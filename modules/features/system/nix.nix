9c94fbd4fa9b0a18e2bbcda20c1527cc2ef6ec9e
Ben <65131969+CyberDie22@users.noreply.github.com>
2026-09-22 20:24:26 -0700

    nix gc

--- modules/features/system/nix.nix
+++ modules/features/system/nix.nix
@@ -1,8 +1,21 @@
 {
   flake.modules.nixos.base = {
-    nix.settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+    nix.settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      trusted-users = [ "@wheel" ];
+
+      min-free = 5 * 1024 * 1024 * 1024;
+      max-free = 20 * 1024 * 1024 * 1024;
+    };
+    nix.optimise.automatic = true;
+
+    programs.nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = "--keep 5 --keep-since 7d";
+    };
   };
 }
