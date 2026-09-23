4ad7b125f706597f167994e509435bc212d23df6
Ben <65131969+CyberDie22@users.noreply.github.com>
2026-09-22 17:48:09 -0700

    add swapfile

--- modules/hosts/testvm.nix
+++ modules/hosts/testvm.nix
@@ -14,6 +14,9 @@ in
         i18n.defaultLocale = "en_US.UTF-8";
         nixpkgs.hostPlatform = "x86_64-linux";
         system.stateVersion = "26.05";
+
+        services.qemuGuest.enable = true;
+        services.spice-vdagentd.enable = true;
       }
     ];
   };
